d86b7f3f0b7fb0368eaff95a210a05064b590ee4
Idriss-Fikri <fikri.idriss12@gmail.com>
2024-03-26 18:41:16 +0100

    userservice

---
+++ src/main/java/com/example/reviewsmysql/service/userService.java
@@ -0,0 +1,49 @@
+package com.example.reviewsmysql.service;
+
+import com.example.reviewsmysql.Exception.EmailNullException;
+import com.example.reviewsmysql.Exception.InvalidEmailException;
+import com.example.reviewsmysql.Exception.UserAlreadyExist;
+import com.example.reviewsmysql.TypeRole;
+import com.example.reviewsmysql.entity.Role;
+import com.example.reviewsmysql.entity.User;
+import com.example.reviewsmysql.repository.UserRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class userService {
+    private  final UserRepo userRepo ;
+    private BCryptPasswordEncoder passwordEncoder ;
+
+    public  void  SignUser(User user ){
+        ValidatorsField( user);
+        String password = user.getPassword();
+        String passwordEncode =  this.passwordEncoder.encode(password);
+        user.setPassword(passwordEncode);
+        Role role=new Role();
+        role.setRole(TypeRole.USER ); // somene who sign he is a user
+        user.setRole(role);
+        this.userRepo.save(user);
+    }
+
+    public  void  ValidatorsField(User user){
+        Optional<User> userfind = this.userRepo.findByEmail(user.getEmail());
+        if ( userfind.isPresent() ) {
+            throw  new UserAlreadyExist();
+        }
+        if ( !user.getEmail().contains("@") ){
+            throw  new InvalidEmailException();
+        }
+        if ( !user.getEmail().contains(".") ){
+            throw  new InvalidEmailException();
+        }
+        if ( user.getEmail().isBlank()){
+            throw  new EmailNullException();
+        }
+
+    }
+}
