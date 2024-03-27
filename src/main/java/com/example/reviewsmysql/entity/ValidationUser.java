ed6ddd21c7ea2d3af9f6502cbcfd54119eb7c25a
Idriss-Fikri <fikri.idriss12@gmail.com>
2024-03-27 09:37:30 +0100

    Creation A validation User  , that when a user is signed  , we validate tha tuser in our  data base

---
+++ src/main/java/com/example/reviewsmysql/entity/ValidationUser.java
@@ -0,0 +1,32 @@
+package com.example.reviewsmysql.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "ValidationUser")
+public class ValidationUser {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long Id ;
+    @Column(name = "date de creation ")
+    Instant CreationDate ;
+    @Column(name = "date d'expiration ")
+    Instant ExpireDate ;
+    @OneToOne(cascade = CascadeType.ALL)
+    User user ;
+    @Column(name = "code d'activation")
+    Integer ActivationCode ;
+
+}
